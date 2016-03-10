ccc88ff327858871ee7fe89ac9a529a48122b4d5
BillelGuerfa <cb_guerfa@esi.dz>
2016-03-10 19:24:40 +0100

    added todo

---
+++ app/src/main/java/dz/esi/activities/DetailFragment.java
@@ -0,0 +1,21 @@
+package dz.esi.activities;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Billel Guerfa on 07/03/2016.
+ */
+
+//TODO DetailActivity views here , use Bundle
+public class DetailFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
